d84207f7b2df0c2b20dcaa5c5c7c4a97bf910cc2
Jonathan Thom and Marty Kovach <marty@>
2016-11-15 09:13:03 -0800

    Translate a two digit, non teen number into a word

--- lib/num_to_words.rb
+++ lib/num_to_words.rb
@@ -1,5 +1,7 @@
 class Fixnum
   define_method(:num_to_words) do
+    digits = self.to_s.size
+
     singles = {
       1 => "one",
       2 => "two",
@@ -12,6 +14,38 @@ class Fixnum
       9 => "nine",
       0 => "",
     }
-    singles.fetch(self)
+
+    tens = {
+      10 => "ten",
+      11 => "eleven",
+      12 => "twelve",
+      13 => "thirteen",
+      14 => "fourteen",
+      15 => "fifteen",
+      16 => "sixteen",
+      17 => "seventeen",
+      18 => "eighteen",
+      19 => "nineteen",
+      2 => "twenty",
+      3 => "thirty",
+      4 => "forty",
+      5 => "fifty",
+      6 => "sixty",
+      7 => "seventy",
+      8 => "eighty",
+      9 => "ninety",
+      0 => "",
+    }
+    if digits.eql?(1)
+      singles.fetch(self)
+    elsif digits.eql?(2)
+      string_number = self.to_s
+      numbers = string_number.split("")
+      tens_word = tens.fetch(numbers[0].to_i)
+      singles_word = singles.fetch(numbers[1].to_i)
+      words = tens_word.concat(" " + singles_word)
+    end
+
+
   end
 end
--- spec/num_to_word_spec.rb
+++ spec/num_to_word_spec.rb
@@ -5,4 +5,8 @@ describe('Fixnum#num_to_words') do
   it('Will tranlate a single digit number to a word') do
     expect(4.num_to_words()).to(eq("four"))
   end
+
+  it('Will tranlate a two digit, non teen number to a word') do
+    expect(84.num_to_words()).to(eq("eighty four"))
+  end
 end
